376dc499e765a4b46acccb61a38bb404835f0967
tendrynyavo <tendrirazafimiandrisoa@gmail.com>
2023-03-14 09:36:44 +0300

    initial

---
+++ src/main/java/etu2074/framework/servlet/FrontServlet.java
@@ -0,0 +1,83 @@
+package etu2074.framework.servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Vector;
+
+@WebServlet(name = "*",value = "/*")
+public class FrontServlet extends HttpServlet {
+    private HttpServletRequest httpServletRequest;
+    private HttpServletResponse httpServletResponse;
+    public FrontServlet(){}
+
+    public void setHttpServletRequest(HttpServletRequest httpServletRequest) {
+        this.httpServletRequest = httpServletRequest;
+    }
+
+    public HttpServletResponse getHttpServletResponse() {
+        return httpServletResponse;
+    }
+
+    public HttpServletRequest getHttpServletRequest() {
+        return httpServletRequest;
+    }
+
+    public void setHttpServletResponse(HttpServletResponse httpServletResponse) {
+        this.httpServletResponse = httpServletResponse;
+    }
+
+
+    @Override
+    public void init() throws ServletException {
+
+    }
+    public final void dispatch(String URL) {
+        try {
+            getHttpServletRequest().getRequestDispatcher(URL).forward(getHttpServletRequest(),getHttpServletResponse());
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void processRequest(HttpServletRequest request,HttpServletResponse response) throws IOException,ServletException{
+        setHttpServletRequest(request);setHttpServletResponse(response);
+        Vector<String> stringVector = retrieveRequestUrl(request);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException,ServletException{
+        processRequest(request,response);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException,ServletException{
+        processRequest(request,response);
+    }
+
+    private Vector<String>retrieveRequestUrl(HttpServletRequest request){
+        String requestURI = request.getRequestURI();
+        String [] linkURI=requestURI.split("/");
+        Vector<String> stringVector = new Vector<>();
+        for (String t: linkURI) {
+            stringVector.add(t);
+        }
+        stringVector.remove(0);
+        stringVector.remove(0);
+        return stringVector;
+    }
+
+
+    private String requestURL(HttpServletRequest request){
+        Vector<String> requestURL = retrieveRequestUrl(request);
+        String ur = "";
+        for (String url:requestURL) {
+            ur+=url;
+        }
+        return ur;
+    }
+}
